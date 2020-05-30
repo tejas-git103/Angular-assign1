d657bc35f1ac2bea69ffa15b0219cfbc5954764d
tejas-git103 <tejasapte47@gmail.com>
2020-05-30 17:04:44 +0530

    final version

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
 (function() {
-  angular.module("Assignment1", [])
-  .controlller("controlller",function ($scope) {
-    $scope.name="tejas";
-  });
+  'use strict';
+  angular.module("LunchCheck", []).
+  controller("LunchCheckController",LunchCheckController);
+  LunchCheckController.$project = ['$scope'];
+  function LunchCheckController($scope)
+  {
+    $scope.name ="";
+    $scope.error="";
+    $scope.result="";
+    $scope.checking =function () {
+      if ($scope.name.localeCompare("")==0) {
+        $scope.result="";
+        $scope.error = "Please enter data first";
+      }
+      else {
+        $scope.error ="";
+        var array=$scope.name.split(",");
+
+        if(array.length>3)
+        {
+          $scope.result="Too much!";
+        }
+        else {
+          $scope.result="Enjoy!";
+        }
+      }
+    }
+  }
 
 }());
